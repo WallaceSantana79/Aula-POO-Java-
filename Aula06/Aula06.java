7f7d56b43b76cb3cfd4dc44b4e75715143b5a672
Wallace Santana <wallace.santana@souunit.com.br>
2023-05-17 09:18:57 -0300

    Aula 06

---
+++ Aula06/Aula06.java
@@ -0,0 +1,11 @@
+package Aula06;
+
+public class Aula06 {
+    public static void main(String [] args) {
+        ControleRemoto c = new ControleRemoto(0, false, false);
+        c.ligar();
+        c.maisVolume();
+        c.abrirMenu();
+    }
+    
+}
